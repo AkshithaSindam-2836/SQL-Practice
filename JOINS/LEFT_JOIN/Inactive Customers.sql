972e9c4087585bcea28570486368c46fc2a1562d
Sindam Akshitha <sindamakshitha@gmail.com>
2026-09-14 19:53:31 +0530

    Update Inactive Customers.sql

--- JOINS/LEFT_JOIN/Inactive Customers.sql
+++ JOINS/LEFT_JOIN/Inactive Customers.sql
@@ -2,4 +2,36 @@
 
 Given two tables, `Customers` and `Orders`, write an SQL query to find the names of all customers who have **never placed an order**.
 
+**Example**
 
+**Input**
+
+**Customers**
+
+| **id** | **name** |
+| :--- | :--- |
+| 1 | Joe |
+| 2 | Henry |
+| 3 | Sam |
+| 4 | Max |
+
+**Orders**
+
+| **id** | **customerId** |
+| :--- | :--- |
+| 1 | 3 |
+| 2 | 1 |
+
+**Output**
+
+| **Customers** |
+| :--- |
+| Henry |
+| Max |
+
+SOLUTION : 
+SELECT c.name AS Customers
+FROM Customers c
+LEFT JOIN Orders o
+ON c.id = o.customerId
+WHERE o.id IS NULL;
